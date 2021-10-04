edcb1466668c8988c1cd981e69d8402eddbc7de7
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-10-04 12:39:30 -0300

    Added Alive conditionals

--- Conditionals2/Program.cs
+++ Conditionals2/Program.cs
@@ -8,8 +8,34 @@ namespace Conditionals2
 {
 	class Program
 	{
+		static int score = 0;
+		static int Lives = 3;
+		static bool Alive = true; // true if alive, false if dead
+
+
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Conditionals2");
+
+			Lives = Lives - 1; // player lost life
+			if (Lives < 0)
+			{
+				Lives = 0; // range check it to 0
+				Alive = false;
+
+			}
+
+
+			if (Lives == 0)
+			{
+
+				Alive = false;
+
+			}
+
+			Console.ReadKey(true);
 		}
+
+
 	}
 }
